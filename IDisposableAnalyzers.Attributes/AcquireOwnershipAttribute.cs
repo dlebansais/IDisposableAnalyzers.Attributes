3e9f2ae10db623689988a34070c54a831246273a
David Le Bansais <dlebansais@gmail.com>
2025-06-15 19:06:51 +0200

    Add project files.

---
+++ IDisposableAnalyzers.Attributes/AcquireOwnershipAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace IDisposableAnalyzers;
+
+using System;
+
+/// <summary>
+/// Represents an attribute indicating that a parameter of a disposable type acquires ownership of the provided value.
+/// </summary>
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+public sealed class AcquireOwnershipAttribute : Attribute
+{
+}
